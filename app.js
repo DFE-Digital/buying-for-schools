d6dfdeb11b798e19b4d48dd9d1568f35723eced7
s6stuc <s.craigen@mac.com>
2019-02-20 10:42:37 +0000

    Azure test module installs

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require('http')
 const port = process.env.PORT || 3000
+const nunjucks = require('nunjucks')
+const path = require('path')
+nunjucks.configure([
+  path.resolve(__dirname, './app/templates'),
+  path.resolve(__dirname, './node_modules/govuk-frontend/'),
+  path.resolve(__dirname, './node_modules/govuk-frontend/components/')
+], {
+  autoescape: true
+})
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello World</h1>')
+  res.end(nunjucks.render('buying-for-schools.njk'))
 })
 server.listen(port, () => {
   console.log(`Server running at port ` + port)
