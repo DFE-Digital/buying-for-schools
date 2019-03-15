6b58d8ed20ad7e90cf3d8145177f13cf6bcdf7f4
s6stuc <s.craigen@mac.com>
2019-03-15 14:23:21 +0000

    Added cucumber to the gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ function jestTest () {
 }
 
 function build (done) {
-  return gulp.series(assets, sass, jestTest)(done)
+  return gulp.series(assets, sass, jestTest, cucumber)(done)
 }
 
 function cucumber(done) {
