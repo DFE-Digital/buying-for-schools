6e332c917f06af455f56a1f4d3adbcb9543fb296
s6stuc <s.craigen@mac.com>
2019-03-20 09:15:31 +0000

    Added puppeteer test file

---
+++ puppeteer.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+
+  const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
+
+  console.log('Dimensions:', dimensions);
+  console.log('Title:', title);
+
+  await browser.close();
+})();
