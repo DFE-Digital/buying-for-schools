95644fa9a05d0fa1a2cba7f6f2b65f6ebfbfb0cd
s6stuc <s.craigen@mac.com>
2019-03-20 16:45:38 +0000

    Jest puppeteer

--- e2e.test.js
+++ e2e.test.js
@@ -3,39 +3,16 @@
 const puppeteer = require('puppeteer')
 const { server } = require('./app/index')
 const SERVICE_URL = 'http://localhost:5000'
-let browser = null
-let page = null
-
-beforeAll(async () => {
-  browser = await puppeteer.launch()
-  page = await browser.newPage()
-})
 
 describe('e2e', () => {
-  const urls = [
-    '/',
-    '/selection',
-    '/service-output',
-    '/frameworks',
-    '/frameworks/type',
-    '/frameworks/type',
-    '/frameworks/type/buying/what',
-    '/frameworks/type/buying/what/books-media/class-library',
-    '/frameworks/type/buying/what/books-media/class-library/classroom/books'
-  ]
+  it('should show page with h1', async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.bbc.co.uk');
+    const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
+    expect(title).toBe('BBC Homepage')
 
-  urls.forEach(u => {
-    it(`should show page with h1: ${u}`, async () => {
-      const onpage = await page.goto(SERVICE_URL + u)
-      const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
-      console.log(title)
-      expect(typeof title).toBe('string')
-    })
+    await server.close()
+    await browser.close()
   })
-})
-
-afterAll(async () => {
-  console.log('afterAll')
-  await browser.close()
-  await server.close()
 })
