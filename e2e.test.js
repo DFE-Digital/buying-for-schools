bf40a75a9f2fc2cbdf8cc8d9a26334f9cf7e0cae
s6stuc <s.craigen@mac.com>
2019-03-21 14:03:12 +0000

    Extended timer debugging

--- e2e.test.js
+++ e2e.test.js
@@ -6,17 +6,29 @@ const SERVICE_URL = 'http://localhost:5000'
 
 jest.setTimeout(30000);
 
+const et = (msg, starttime) => {
+  const elapsed = new Date().getTime() - starttime
+  console.log(msg, elapsed)
+}
+
 describe('e2e', () => {
   it('should show page with h1', (done) => {
     ( async () => {
+      const t = new Date().getTime()
       const browser = await puppeteer.launch()
+      et('Browser launched', t)
       const page = await browser.newPage()
+      et('New page ready', t)
       await page.goto('https://www.bbc.co.uk');
+      et('At target page', t)
       const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
+      et('got title', t)
       expect(title).toBe('BBC Homepage')
-
+      et('completed test', t)
       await server.close()
+      et('server closed', t)
       await browser.close()
+      et('browser closed', t)
       done()
     })()
   })
