ac7fa32bddd9ad8517492644048b829ef77157aa
s6stuc <s.craigen@mac.com>
2019-03-21 10:39:28 +0000

    async done

--- e2e.test.js
+++ e2e.test.js
@@ -5,15 +5,17 @@ const { server } = require('./app/index')
 const SERVICE_URL = 'http://localhost:5000'
 
 describe('e2e', () => {
-  it('should show page with h1', async (done) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.bbc.co.uk');
-    const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
-    expect(title).toBe('BBC Homepage')
+  it('should show page with h1', (done) => {
+    ( async () => {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto('https://www.bbc.co.uk');
+      const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
+      expect(title).toBe('BBC Homepage')
 
-    await server.close()
-    await browser.close()
-    done()
+      await server.close()
+      await browser.close()
+      done()
+    })()
   })
 })
