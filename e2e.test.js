dcac0ab364baaad050e1bf012a5482fb2eaae80d
s6stuc <s.craigen@mac.com>
2019-03-21 13:35:24 +0000

    Extended timer

--- e2e.test.js
+++ e2e.test.js
@@ -4,18 +4,20 @@ const puppeteer = require('puppeteer')
 const { server } = require('./app/index')
 const SERVICE_URL = 'http://localhost:5000'
 
-// describe('e2e', () => {
-//   it('should show page with h1', (done) => {
-//     ( async () => {
-//       const browser = await puppeteer.launch()
-//       const page = await browser.newPage()
-//       await page.goto('https://www.bbc.co.uk');
-//       const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
-//       expect(title).toBe('BBC Homepage')
+jest.setTimeout(10000);
 
-//       await server.close()
-//       await browser.close()
-//       done()
-//     })()
-//   })
-// })
+describe('e2e', () => {
+  it('should show page with h1', (done) => {
+    ( async () => {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto('https://www.bbc.co.uk');
+      const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
+      expect(title).toBe('BBC Homepage')
+
+      await server.close()
+      await browser.close()
+      done()
+    })()
+  })
+})
