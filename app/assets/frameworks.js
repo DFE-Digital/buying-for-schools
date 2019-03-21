fc7a63e3409520e547599b4dc3e91e5f3130a8c9
s6stuc <s.craigen@mac.com>
2019-03-21 11:54:26 +0000

    Changed ga to gtag for event tracking

--- app/assets/frameworks.js
+++ app/assets/frameworks.js
@@ -1,13 +1,28 @@
 /* global ga */
 
-function handleOutboundLinkClicks(event) {
-  console.log(event.target.href)
-  ga('send', 'event', 'Framework link', 'click', event.target.href)
+/**
+* Function that registers a click on an outbound link in Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label. Setting the transport method to 'beacon' lets the hit be sent
+* using 'navigator.sendBeacon' in browser that support it.
+*/
+var getOutboundLink = function(e) {
+  e.preventDefault()
+  var url = e.target.href
+  gtag('event', 'click', {
+    'event_category': 'outbound',
+    'event_label': url,
+    'transport_type': 'beacon',
+    'event_callback': function(){
+      document.location = url
+    }
+  });
+  return false;
 }
 
 var externalButtons = document.getElementsByClassName('frameworkbutton--external')
 
 for (var i=0; i<externalButtons.length; i++) {
-  externalButtons[i].onclick = handleOutboundLinkClicks
+  externalButtons[i].onclick = getOutboundLink
 }
 
