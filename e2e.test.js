59f87798665cfa54d5f82e8dfe91c91a29204803
s6stuc <s.craigen@mac.com>
2019-03-21 14:10:27 +0000

    Extended timer debugging

--- e2e.test.js
+++ e2e.test.js
@@ -1,34 +1,41 @@
 /* global describe it expect afterAll */
-
-const puppeteer = require('puppeteer')
-const { server } = require('./app/index')
-const SERVICE_URL = 'http://localhost:5000'
-
-jest.setTimeout(30000);
-
-const et = (msg, starttime) => {
+const starttime = new Date().getTime()
+const et = (msg) => {
   const elapsed = new Date().getTime() - starttime
   console.log(msg, elapsed)
 }
 
+
+et('START')
+const puppeteer = require('puppeteer')
+
+const { server } = require('./app/index')
+et('Service started')
+
+const SERVICE_URL = 'http://localhost:5000'
+
+jest.setTimeout(30000);
+
+
+
 describe('e2e', () => {
   it('should show page with h1', (done) => {
     ( async () => {
-      const t = new Date().getTime()
+      
       const browser = await puppeteer.launch()
-      et('Browser launched', t)
+      et('Browser launched')
       const page = await browser.newPage()
-      et('New page ready', t)
+      et('New page ready')
       await page.goto('https://www.bbc.co.uk');
-      et('At target page', t)
+      et('At target page')
       const title = await page.evaluate((s) => document.querySelector(s).innerText, 'h1')
-      et('got title', t)
+      et('got title')
       expect(title).toBe('BBC Homepage')
-      et('completed test', t)
+      et('completed test')
       await server.close()
-      et('server closed', t)
+      et('server closed')
       await browser.close()
-      et('browser closed', t)
+      et('browser closed')
       done()
     })()
   })
