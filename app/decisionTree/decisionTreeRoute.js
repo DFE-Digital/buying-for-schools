0f9f0c0646cf54bd975e9d7b86e1d487a23285e4
s6stuc <s.craigen@mac.com>
2019-03-28 16:40:16 +0000

    Removed unused code

--- app/decisionTree/decisionTreeRoute.js
+++ app/decisionTree/decisionTreeRoute.js
@@ -18,17 +18,6 @@ const getQuestionAnswerPairSlugs = (url) => {
   return fromJS(pairs)
 }
 
-const getUrlFromPairs = (pairs) => {
-  let url = []
-  pairs.forEach((v, k) => {
-    url.push(k)
-    if (v) {
-      url.push(v)
-    }
-  })
-  return url.join('/')
-}
-
 const getBranchPath = (tree, pairs) => {
   let branches = List()
   pairs.forEach((answerRef, questionRef) => {
@@ -39,82 +28,6 @@ const getBranchPath = (tree, pairs) => {
     }
   })
   return branches
-}
-
-const validateQuestionAnswerPairs = (tree, pairs, frameworks = []) => {
-  const questionRefs = pairs.keySeq(k => k)
-  const answerRefs = pairs.valueSeq(v => v)
-  if (questionRefs.size === 0) {
-    return false
-  }
-  return questionRefs.every((questionRef, i) => {
-    const branch = dt.getBranch(tree, questionRef)
-    const answerRef = answerRefs.get(i)
-    const result = dtres.getFramework(frameworks, questionRef)
- 
-    if (result) {
-      return true
-    }
-
-    if (!branch) {
-      // no branch with given ref
-      return false
-    }
-
-    if (answerRef === undefined) {
-      // question branch is valid but not answered
-      return true
-    }
-
-    const answeredOption = dt.getOption(branch, answerRef)
-    if (!answeredOption) {
-      // answer given was not available here
-      return false
-    }
-
-    if (i === questionRefs.size - 1) {
-      // at the end of the question pairs
-      return true
-    }
-
-    const results = answeredOption.get('result')
-    if (results && results.includes(questionRefs.get(i + 1))) {
-      return true
-    }
-
-    const next = answeredOption.get('next')
-    if (next !== questionRefs.get(i + 1)) {
-      // answer does not match with the next question
-      return false
-    }
-
-    // default
-    return true
-  })
-}
-
-const getSanitisedQuestionAnswerPairs = (tree, pairs) => {
-  const questionRefs = pairs.keySeq(k => k)
-  const answerRefs = pairs.valueSeq(v => v)
-  let newPairs = pairs
-  if (answerRefs.last() === undefined) {
-    // last question was unanswered so if here then the question itself must be invalid
-    newPairs = pairs.delete(questionRefs.last())
-  } else {
-    newPairs = pairs.set(questionRefs.last(), undefined)
-  }
-
-  if (newPairs.size === 0) {
-    const treeStart = tree.first().get('ref')
-    return Map().set(treeStart, undefined)
-  }
-
-  const valid = validateQuestionAnswerPairs(tree, newPairs)
-  if (valid) {
-    return newPairs
-  }
-
-  return getSanitisedQuestionAnswerPairs(tree, newPairs)
 }
 
 const getQuestionAnswerSummary = (branchPath, baseUrl) => {
@@ -145,9 +58,6 @@ const getQuestionAnswerSummary = (branchPath, baseUrl) => {
 
 module.exports = {
   getQuestionAnswerPairSlugs,
-  getUrlFromPairs,
   getBranchPath,
-  validateQuestionAnswerPairs,
-  getSanitisedQuestionAnswerPairs,
   getQuestionAnswerSummary
 }
