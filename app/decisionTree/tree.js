6c1f54ed9ed01d64e37e9532d9db9feca7f345d0
s6stuc <s.craigen@mac.com>
2019-04-10 15:35:41 +0100

    Refactor WIP

--- app/decisionTree/tree.js
+++ app/decisionTree/tree.js
@@ -1,28 +1,13 @@
-const path = require('path')
-
-const uniq = arr => {
-  return arr.filter((v, i, self) => self.indexOf(v) === i)
-}
-
-// const deepFreeze = (obj) => {
-//   const propNames = Object.getOwnPropertyNames(obj)
-//   for (let name of propNames) {
-//     const value = obj[name]
-//     obj[name] = (value && typeof value === "object") ? deepFreeze(value) : value
-//   }
-//   return Object.freeze(obj)
-// }
+const treeUtils = require('./treeUtils')
 
 const tree = (data) => {
   const branches = data.map(branchdata => makeBranch(branchdata))
   const tree = {
     getFirst: () => branches[0],
     getBranches: () => branches,
-    getBranch: (branchRef) => {
-      return branches.find(branch => branch.getRef() === branchRef)
-    }
+    getBranch: (branchRef) => branches.find(branch => branch.getRef() === branchRef)
   }
-  const allPaths = getAllBranchPaths(tree)
+  const allPaths = treeUtils.getAllBranchPaths(tree)
   tree.getAllPaths = () => allPaths
   return tree
 }
@@ -31,13 +16,11 @@ const makeBranch = (data) => {
   const { ref, title, err } = data
   const options = data.options.map(optiondata => makeOption(optiondata))
   return {
-    getRef:     () => ref,
-    getTitle:   () => title,
-    getErr:     () => err,
+    getRef: () => ref,
+    getTitle: () => title,
+    getErr: () => err,
     getOptions: () => options,
-    getOption: (optionRef) => {
-      return options.find(option => option.getRef() === optionRef)
-    },
+    getOption: (optionRef) => options.find(option => option.getRef() === optionRef),
     toObject: () => {
       const jsOptions = options.map(opt => opt.toObject())
       return {
@@ -53,11 +36,11 @@ const makeBranch = (data) => {
 const makeOption = (data) => {
   const { ref, title, next, result } = data
   return {
-    getRef:    () => ref,
-    getTitle:  () => title,
-    getNext:   () => next,
+    getRef: () => ref,
+    getTitle: () => title,
+    getNext: () => next,
     getResult: () => result,
-    toObject:  () => {
+    toObject: () => {
       return {
         ref,
         title,
@@ -68,48 +51,16 @@ const makeOption = (data) => {
   }
 }
 
-const getAllBranchPaths = (tree) => {
-  const questionPaths = []
-  const redirectToQuestionPaths = []
-  const redirectToResultPaths = []
-  const resultPaths = []
-  const multiplePaths = []
- 
-  const recursion = (basePath, qref) => {
-    const branch = tree.getBranch(qref)
-    const questionPath = path.join(basePath, qref)
-    const options = branch.getOptions()
-    questionPaths.push(questionPath)
-    options.forEach(opt => {
-      const answerPath = path.join(questionPath, opt.getRef())
-      const nxt = opt.getNext()
-      const results = opt.getResult()
-      if (nxt) {
-        redirectToQuestionPaths.push(answerPath)
-        recursion(answerPath, nxt)
-      }
-      if (results) {
-        if (results.length === 1) {
-          redirectToResultPaths.push(answerPath)
-        } else {
-          multiplePaths.push(answerPath)
-        }
-        results.forEach(res => {
-          resultPaths.push(path.join(answerPath, res))
-        })
-      }
-    })
-  }
-
-  recursion('', tree.getFirst().getRef())
-  return {
-    questions: uniq(questionPaths),
-    redirectToQuestion: uniq(redirectToQuestionPaths),
-    redirectToResult: uniq(redirectToResultPaths),
-    results: uniq(resultPaths),
-    multiple: uniq(multiplePaths)
-  }
-}
-
-
 module.exports = tree
+
+
+
+
+// const deepFreeze = (obj) => {
+//   const propNames = Object.getOwnPropertyNames(obj)
+//   for (let name of propNames) {
+//     const value = obj[name]
+//     obj[name] = (value && typeof value === "object") ? deepFreeze(value) : value
+//   }
+//   return Object.freeze(obj)
+// }
---
+++ app/decisionTree/treeUtils.js
@@ -0,0 +1,90 @@
+const path = require('path')
+
+const uniq = arr => {
+  return arr.filter((v, i, self) => self.indexOf(v) === i)
+}
+
+const getAllBranchPaths = (tree) => {
+  const questionPaths = []
+  const redirectToQuestionPaths = []
+  const redirectToResultPaths = []
+  const resultPaths = []
+  const multiplePaths = []
+ 
+  const recursion = (basePath, qref) => {
+    const branch = tree.getBranch(qref)
+    const questionPath = path.join(basePath, qref)
+    const options = branch.getOptions()
+    questionPaths.push(questionPath)
+    options.forEach(opt => {
+      const answerPath = path.join(questionPath, opt.getRef())
+      const nxt = opt.getNext()
+      const results = opt.getResult()
+      if (nxt) {
+        redirectToQuestionPaths.push(answerPath)
+        recursion(answerPath, nxt)
+      }
+      if (results) {
+        if (results.length === 1) {
+          redirectToResultPaths.push(answerPath)
+        } else {
+          multiplePaths.push(answerPath)
+        }
+        results.forEach(res => {
+          resultPaths.push(path.join(answerPath, res))
+        })
+      }
+    })
+  }
+
+  recursion('', tree.getFirst().getRef())
+  return {
+    questions: uniq(questionPaths),
+    redirectToQuestion: uniq(redirectToQuestionPaths),
+    redirectToResult: uniq(redirectToResultPaths),
+    results: uniq(resultPaths),
+    multiple: uniq(multiplePaths)
+  }
+}
+
+const getBranchPath = (tree, pairs) => {
+  let branches = List()
+  pairs.forEach((answerRef, questionRef) => {
+    let branch = tree.getBranch(questionRef)
+    if (branch) {
+      branch = dt.setSelectedOption(branch, answerRef)
+      branches = branches.push(branch)
+    }
+  })
+  return branches
+}
+
+const getQuestionAnswerSummary = (tree, pairs, baseUrl) => {
+  let summary = List()
+  let url = List()
+  pairs.forEach((answerRef, questionRef) => {
+    const branch = tree.getBranch(questionRef)
+    const selectedOption = branch.getOption(answerRef)
+    url = url.push(questionRef)
+    if (selectedOption) {
+      summary = summary.push({
+        key: { text: branch.getTitle() },
+        value: { text: selectedOption.getTitle() },
+        actions: {
+          items: [{
+            href: baseUrl + '/' + url.join('/'),
+            text: 'Change',
+            visuallyHiddenText: branch.getTitle()
+          }]
+        }
+      })
+      url = url.push(selectedOption.getRef())
+    }
+  })
+
+  return summary
+}
+
+module.exports = {
+  getAllBranchPaths
+}
